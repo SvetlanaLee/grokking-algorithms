af8f3d2c1c492c9ab8ceb6bc0fd979a3a983a215
Svetlana <lirica1109@gmail.com>
2022-08-23 20:31:54 +0300

    recursion

---
+++ recursion.js
@@ -0,0 +1,13 @@
+// Написать функцию, которая будет высчитывать факториал
+
+function factorial(number) {
+ 
+  if(number <= 1) {
+    return 1
+  } else {
+    return number * factorial(number - 1)
+  }
+
+}
+
+console.log(factorial(5))
